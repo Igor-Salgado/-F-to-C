0e6ae8221812d9373e015b1a6a4151cfbbdf2ef8
Igor Salgado <igorcesar95@hotmail.com>
2025-04-07 17:22:56 +0000

    Fix

--- temperatureFtoC.py
+++ temperatureFtoC.py
@@ -1,4 +1,4 @@
 Fahrenheit = int(input("Type the temperature in °F:\n \n"))
 Celsius = int((Fahrenheit -32)/1.8) #The Fahrenheit scale was created by the European physicist Daniel Gabriel Fahrenheit (1686–1736).
 #The Celsius scale was created by the astronomer, physicist, and geophysicist Anders Celsius (1701-1744).
-print (f"\nThe temperature in Celcius is {Celsius}°C.")
+print (f"\nThe temperature in Celcius is: {Celsius}°C.")
